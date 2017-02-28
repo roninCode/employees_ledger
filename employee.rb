f03b7755018f32574debe5d1dcdc3c455224441c
Jamie MacKillop <jamieronin@gmail.com>
2017-02-27 19:26:45 -0500

    Add 2 employees and prints contents

---
+++ employee.rb
@@ -0,0 +1,6 @@
+employee1 = ["Manila", "Campos", 80000, true]
+employee2 = ["Danilo", "Carter", 70000, false]
+
+# Manila Campos makes $80000 per year
+p "#{employee1[0]} #{employee1[1]} makes $#{employee1[2]} per year"
+p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year"
