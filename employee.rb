4b2c9c47ea0d1561aa718e6c70631d18fe7594f5
Jamie MacKillop <jamieronin@gmail.com>
2017-02-27 19:31:55 -0500

    Add hash with strings

--- employee.rb
+++ employee.rb
@@ -4,3 +4,20 @@ employee2 = ["Danilo", "Carter", 70000, false]
 # Manila Campos makes $80000 per year
 p "#{employee1[0]} #{employee1[1]} makes $#{employee1[2]} per year"
 p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year"
+
+
+# HASH with strings
+employee1 = {
+  "first_name" => "Manila", 
+  "last_name" => "Campos", 
+  "salary" => 80000, 
+  "active_status" => true}
+
+employee2 = {
+  "first_name" => "Danilo", 
+  "last_name" => "Carter", 
+  "salary" => 70000, 
+  "active_status" => false}
+
+  p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+  p "#{employee2['first_name']} #{employee2['last_name']} makes $#{employee2['salary']} per year."
